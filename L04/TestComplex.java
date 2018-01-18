0b3fc6ce7739220d75991ab7be4e780a6b06672e
Timothy J Hickey <tjhickey@brandeis.edu>
2018-01-18 09:41:12 -0500

    version 3

--- L04/TestComplex.java
+++ L04/TestComplex.java
@@ -1,8 +1,13 @@
 public class TestComplex{
   public static void main(String[] args){
-    Complex z = new Complex();
-    z.re=1; z.im=2;
+    Complex z = new Complex(1,2);
+    Complex w = new Complex(3,1);
+    Complex r = new Complex(7);
+    Complex u = Complex.add(z,w);
 
     System.out.println("z="+z);
+    System.out.println("w="+w);
+    System.out.println("r="+r);
+    System.out.println("z+w="+u);
   }
 }
