20cba42134271119920d7ba6fb31c16c9894f5a5
Timothy J Hickey <tjhickey@brandeis.edu>
2018-01-18 09:19:47 -0500

    L04 version 2

--- L04/Complex.java
+++ L04/Complex.java
@@ -1,4 +1,8 @@
 public class Complex{
   public double re;
   public double im;
+
+  public String toString(){
+    return this.re+"+"+this.im+"i";
+  }
 }
--- L04/TestComplex.java
+++ L04/TestComplex.java
@@ -1,6 +1,8 @@
 public class TestComplex{
   public static void main(String[] args){
     Complex z = new Complex();
+    z.re=1; z.im=2;
+
     System.out.println("z="+z);
   }
 }
