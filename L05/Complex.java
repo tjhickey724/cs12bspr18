79626fffa240f56d3a96046f51f48730422aed31
Timothy J Hickey <tjhickey@brandeis.edu>
2018-01-22 09:14:50 -0500

    added multiplication to Complex

--- L05/Complex.java
+++ L05/Complex.java
@@ -17,6 +17,14 @@ public class Complex{
     return z;
   }
 
+  public static Complex multiply(Complex u, Complex v){
+    double real, imag;
+    real = u.re*v.re - u.im*v.im;
+    imag = u.re*v.im + u.im*v.re;
+    Complex w = new Complex(real, imag);
+    return w;
+  }
+
   public String toString(){
     return this.re+"+"+this.im+"i";
   }
