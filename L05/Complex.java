592933133c59bbc8311fcd8ebc7024c29fd9d1c6
Timothy J Hickey <tjhickey@brandeis.edu>
2018-01-22 09:42:23 -0500

    added equals method and getters/setters

--- L05/Complex.java
+++ L05/Complex.java
@@ -1,6 +1,6 @@
 public class Complex{
-  public double re;
-  public double im;
+  private double re;
+  private double im;
 
   public Complex(double re, double im){
     this.re = re;
@@ -12,6 +12,35 @@ public class Complex{
     this.im = 0;
   }
 
+  /**
+  returns the real part of the complex number z
+  called as  double r = z.getReal();
+  */
+  public double getReal(){
+    return this.re;
+  }
+  public double getImag(){
+    return this.im;
+  }
+  /**
+  changes the real part of a complex number z
+  called as z.setReal(1.1);
+  */
+  private void setReal(double re){
+    this.re = re;
+  }
+  private void setImag(double im){
+    this.im=im;
+  }
+
+  public boolean equals(Complex z){
+     return (z.re==this.re) && (z.im==this.im);
+  }
+
+  /**
+  adds two complex numbers to get a complex number
+  called as Complex w = Complex.add(u,v);
+  */
   public static Complex add(Complex u, Complex v){
     Complex z = new Complex(u.re+v.re, u.im+v.im);
     return z;
@@ -25,6 +54,10 @@ public class Complex{
     return w;
   }
 
+  /**
+  returns a string representation of the objext z
+  called as z.toString()
+  */
   public String toString(){
     return this.re+"+"+this.im+"i";
   }
--- L05/TestComplex.java
+++ L05/TestComplex.java
@@ -1,13 +1,20 @@
 public class TestComplex{
   public static void main(String[] args){
     Complex z = new Complex(1,2);
+    Complex z2 = new Complex(1,2);
+    System.out.println("z==z2: "+ (z==z2));
+    System.out.println("z.equals(z2): "+
+         (z.equals(z2)));
     Complex w = new Complex(3,1);
     Complex r = new Complex(7);
     Complex u = Complex.add(z,w);
     Complex v = Complex.multiply(z,w);
 
     System.out.println("z="+z);
+    System.out.println("z.re="+z.getReal());
     System.out.println("w="+w);
+    System.out.println("r="+r);
+    // r.setImag(1.1); // can't do this !!!
     System.out.println("r="+r);
     System.out.println("z+w="+u);
     System.out.println("z*w="+v);
