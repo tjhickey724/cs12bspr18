3d2684f4af52c1f7d96f2a0943a02b6ecce2ade3
Timothy J Hickey <tjhickey@brandeis.edu>
2018-04-18 09:19:16 -0400

    added MapEntry

---
+++ L33/MapEntry.java
@@ -0,0 +1,21 @@
+public class MapEntry
+{
+	public String key;
+	public String value;
+	
+	public MapEntry(String key, String value)
+	{
+		this.key = key;
+		this.value = value;
+	}
+	
+	public String getKey()
+	{
+		return key;
+	}
+	
+	public String getValue()
+	{
+		return value;
+	}
+}
